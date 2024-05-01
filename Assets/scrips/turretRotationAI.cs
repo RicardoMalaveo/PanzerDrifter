92958d071f171498e0d0baff98afcb584ba1b046
RICARDOS-LAPTOP\Ricardo Malavé <ricardomalaveo@gmail.com>
2024-05-01 13:11:04 +0200

    el enemigo dispara solo cuando esta cerca del jugador

--- Assets/scrips/turretRotationAI.cs
+++ Assets/scrips/turretRotationAI.cs
@@ -10,22 +10,22 @@ public class turretRotationAI : MonoBehaviour
     public string enemyTag = "Enemy";
     public Transform Turret;
     public float turretRotationSpeed = 15F;
-    public Transform tankBody;
-
-
-    public float launchSpeed = 100.0f;
     public GameObject objectPrefab;
+    public float fireRate = 1F;
+    private float fireCountDown = 0F;
+    public float launchSpeed = 450F;
+    private AudioSource sonidoDisparo;
+    public Transform bulletspawn;
+
     private void Start()
     {
-        InvokeRepeating("UpdateTarget", 0F, 2F);
+        InvokeRepeating("UpdateTarget", 0F, 0.5F);
     }
     void UpdateTarget()
     {
         GameObject [] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
         float shortestDistance = Mathf.Infinity;
-
         GameObject nearestEnemy = null;
-
         foreach (GameObject enemy in enemies)
         {
             float distanceToEnemy = Vector3.Distance (transform.position, enemy.transform.position);
@@ -58,12 +58,36 @@ public class turretRotationAI : MonoBehaviour
         Quaternion LookRotation = Quaternion.LookRotation(direction);
         Vector3 rotation = Quaternion.Lerp (Turret.rotation, LookRotation, Time.deltaTime * turretRotationSpeed).eulerAngles;
         Turret.rotation = Quaternion.Euler(0F, rotation.y, 0F);
+
+        if (fireCountDown <= 0F)
+        {
+            Shoot();
+            fireCountDown = 1F / fireRate;
+        }
+        fireCountDown -= Time.deltaTime;
     }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, range);
+    }
+    public void Shoot()
+    {
+        sonidoDisparo = GetComponent<AudioSource>();
+        sonidoDisparo.PlayOneShot(sonidoDisparo.clip);
+
+        Vector3 SpawnPosition = bulletspawn.transform.position;
+        Quaternion spawnRotation = Quaternion.identity;
+
+        Vector3 localXDirection = bulletspawn.transform.TransformDirection(Vector3.forward);
+
+        Vector3 velocity = localXDirection * launchSpeed;
+
+        GameObject newObject = Instantiate(objectPrefab, SpawnPosition, spawnRotation);
+
+        Rigidbody rb = newObject.GetComponent<Rigidbody>();
+        rb.velocity = velocity;
     }
 
 }

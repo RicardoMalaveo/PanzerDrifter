0a3c27bfd996fd842a1be7c4c3ba61bd363730d3
RICARDOS-LAPTOP\Ricardo Malavé <ricardomalaveo@gmail.com>
2024-05-23 12:23:00 +0200

    enemigo tiene anti vuelco automatico el minimap tiene dos iconos representando a los jugadores, pero esta bastante feo...

--- Assets/scrips/AIscripts/AIInputManager.cs
+++ Assets/scrips/AIscripts/AIInputManager.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class AIInputManager : MonoBehaviour
 {
+    public GameObject prefabInstance;
+    public float increaseYAmount = 1.0f;
     public float vertical;
     public float horizontal;
     public bool handbreak;
     public AITankWayPoints TankWayPoints;
-
     public List<Transform> nodes = new List<Transform>();
     public Transform currentWayPoint;
     [Range(0, 10)] public int distanceOffset;
     [Range(0, 10)] public float rotationSpeed;
     public int AIcurrentNode;
+    public int nodeCheck = 0;
+    public float flippedTimer = 5F;
 
 
     private void Awake()
@@ -25,6 +28,7 @@ public class AIInputManager : MonoBehaviour
     {
         AIDrive();
         WayPointDistanceCalculator();
+        FlipChecker();
     }
 
     private void AIDrive()
@@ -71,6 +75,30 @@ public class AIInputManager : MonoBehaviour
         catch
         {
         }
+    }
+    public void FlipChecker()
+    {
+        if (nodeCheck == AIcurrentNode) 
+        {
+            flippedTimer-= Time.deltaTime;
+
+            if (flippedTimer <= 0)
+            {
+                Vector3 newPosition = prefabInstance.transform.position;
+                newPosition.y += increaseYAmount;
+                prefabInstance.transform.position = newPosition;
+                float currentRotationY = prefabInstance.transform.rotation.eulerAngles.y;
+                prefabInstance.transform.rotation = Quaternion.Euler(0, currentRotationY, 0);
+
+                flippedTimer = 5F;
+            }
+        }
+        else
+        {
+            nodeCheck = AIcurrentNode;
+            flippedTimer = 5F;
+        }
+
     }
     private void OnDrawGizmos()
     {
--- Assets/scrips/PrefabController.cs
+++ Assets/scrips/PrefabController.cs
@@ -4,32 +4,22 @@ using UnityEngine;
 
 public class PrefabController : MonoBehaviour
 {
-    public GameObject prefabInstance; // La instancia del prefab en la escena
-    public KeyCode triggerKey = KeyCode.Space; // La tecla que disparará la acción
-    public float increaseYAmount = 1.0f; // Cantidad a aumentar en Y
+    public GameObject prefabInstance;
+    public KeyCode triggerKey = KeyCode.Space;
+    public float increaseYAmount = 1.0f; 
 
     void Update()
     {
-        // Verificar si se ha presionado la tecla especificada
         if (Input.GetKeyDown(triggerKey))
         {
             if (prefabInstance != null)
             {
-                // Aumentar la posición en Y
                 Vector3 newPosition = prefabInstance.transform.position;
                 newPosition.y += increaseYAmount;
                 prefabInstance.transform.position = newPosition;
-
-                // Obtener la rotación actual en el eje X
                 float currentRotationY = prefabInstance.transform.rotation.eulerAngles.y;
-
-                // Resetear la rotación manteniendo el eje X
                 prefabInstance.transform.rotation = Quaternion.Euler(0, currentRotationY, 0);
             }
-        }
-        else
-        {
-            Debug.LogWarning("Prefab instance is not assigned.");
         }
     }
 }
